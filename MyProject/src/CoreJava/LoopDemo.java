76e712ee7dd5ba07a77b9166a90d78f2ab62fe50
91750 <91750@LAPTOP-QLHGPTTQ>
2023-02-26 14:02:47 +0530

    loops

---
+++ MyProject/src/CoreJava/LoopDemo.java
@@ -0,0 +1,31 @@
+package CoreJava;
+
+public class LoopDemo {
+
+	public static void main(String[] args) {
+       int i=0;
+        for(i=0;i<=10;i++);
+        {
+    	   System.out.println("hello");
+    	   
+        }
+       
+           /*if(i<15)
+    	     
+    	   System.out.println("i is smaller than 15 and value of i is "+i);
+        }
+      // else
+    	 //  System.out.println("i is greater than 15 and the value of i is: "+i); */
+
+       /* while(true) {
+        	if(i>5)
+        		break;
+        		i++;
+        		
+        
+        System.out.println("*") ;*/
+        
+        }
+      
+
+}
